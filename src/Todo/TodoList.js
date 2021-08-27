d71dc8725c49670dd80abce485cc2df6572c415d
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-08-27 21:17:19 +0300

    add todos

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 const styles = {
   ul: {
-    listStyle: 'none'
+    listStyle: 'none',
+    margin: 0,
+    padding: 0
   }
 }
 
 export default function TodoList() {
   return (
     <ul style={styles.ul}>
-      <li>1</li>
-      <li>2</li>
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
     </ul>
   )
 }
