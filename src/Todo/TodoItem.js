cddc35105f178659c26bb9e68343a324c2c55848
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-08-27 23:24:33 +0300

    start list styling

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function TodoItem({todo, index}) {
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  }
+}
+
+function TodoItem({ todo, index }) {
   return (
-    <li>
-      <strong>{index + 1}</strong>
+    <li style={styles.li}>
+      <span>
+        <input type="checkbox" style={styles.input}/>
+        <strong>{index + 1}</strong>
         {todo.title}
+      </span>
+      <button>&times;</button>
     </li>
   )
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number
+}
+
+export default TodoItem
